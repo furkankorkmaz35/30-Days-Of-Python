c438b2045a0cb50989a840e2e09a7c6cec93fe66
Furkan <fkorkmaz3543@gmail.com>
2026-09-15 13:03:28 +0300

    Day 1 alistirmaları ve gorevler tamamlandi

---
+++ my_solutions/day01.py
@@ -0,0 +1,50 @@
+"""print("Hello World!")
+
+print(2 + 3)   # addition(+)
+print(3-1)
+print(4*3)
+print(10/5)
+print(4**2)
+print(10%3)
+print(10//3)
+
+print(type(10))                  # Int
+print(type(3.14))                # Float   
+print(type(1 + 3j))              # Complex
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name': 'Asabeneh'}))  # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type(True))                 # Boolean
+print(type(False))                # Boolean 
+
+
+
+print("Furkan Korkmaz Izmir, Turkey 1.76m 68kg 20 years old")
+
+print(type("Furkan Korkmaz"))  # String
+print(type("Izmir, Turkey"))  # String
+print(type(1.76))  # Float
+print(type(68))  # Int
+print(type(20))  # Int
+
+
+print(17/4)
+print(17//4)
+print(17%4)
+
+print(type(17/4))
+print(type(17//4))
+print(type(17%4))
+
+"""
+
+
+print((((2-10)**2)+(3-8)**2)**0.5)
+
+
+print(type(("Python","Java","C#")))
+print(type(["Elma","Armut","Muz"]))
+print(type({"name":"Furkan","role":"Software Engineer"}))
+print(type({"Backend","Frontend","Fullstack"}))
